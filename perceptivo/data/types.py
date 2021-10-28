8fe0c1ba31a9cf0c0621833055fce658426316de
sneakers-the-rat <JLSaunders987@gmail.com>
2021-10-28 15:21:19 -0700

    some examples in docstrings

--- perceptivo/data/types.py
+++ perceptivo/data/types.py
@@ -133,13 +133,34 @@ class Threshold:
 @dataclass
 class Audiogram:
     """
-    A collection of :class:`.Threshold`s that represent a patient's audiogram
+    A collection of :class:`.Threshold`s that represent a patient's audiogram.
+
+    Thresholds can be accessed like a dictionary, using frequencies as keys, eg::
+
+        >>> agram = Audiogram([Threshold(1000, 10), Threshold(2000, 20)])
+        >>> agram[1000]
+        Threshold(frequency=1000, threshold=10, confidence=0)
+        >>> agram[3000] = Threshold(3000, 30)
+        >>> agram[3000]
+        Threshold(frequency=1000, threshold=10, confidence=0)
     """
     thresholds: typing.List[Threshold]
 
     @property
     def frequencies(self) -> typing.List[float]:
+        """List of frequencies in :attr:`.thresholds`"""
         return [thresh.frequency for thresh in self.thresholds]
+
+    def to_dict(self) -> typing.Dict[float, float]:
+        """
+        Return audiogram thresholds as a {frequency:threshold} dictionary, eg.::
+
+            >>> agram = Audiogram([Threshold(1000, 10), Threshold(2000, 20)])
+            >>> agram.to_dict()
+            {1000: 10, 2000: 20}
+
+        """
+        return {thresh.frequency:thresh.threshold for thresh in self.thresholds}
 
     def __getitem__(self, key:float) -> Threshold:
         thresh = [thresh for thresh in self.thresholds if thresh.frequency == key]
