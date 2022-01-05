b5eacfe9fc3625e53dd8752b4f25efe960301921
sneakers-the-rat <JLSaunders987@gmail.com>
2022-01-04 20:10:20 -0800

    backporting tyyping

--- perceptivo/__init__.py
+++ perceptivo/__init__.py
@@ -3,6 +3,13 @@ if sys.version_info.minor >= 8:
     from importlib.metadata import version
 else:
     from importlib_metadata import version
+
+    # monkeypatch typing
+    import typing
+    from typing_extensions import Literal
+    typing.Literal = Literal
+
+
 __version__ = version('perceptivo')
 
 
